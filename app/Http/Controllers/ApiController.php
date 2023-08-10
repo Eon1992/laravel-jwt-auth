77bdfe55b97c7c72fec79846a11c28dd138e3fa3
Eon1092 <rahul.arora@aurosociety.org>
2023-08-10 09:46:23 +0530

    changes regrading error code done

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,6 +11,9 @@ use Illuminate\Support\Facades\Validator;
 
 class ApiController extends Controller
 {
+    protected $response = [];
+    protected $error_msg = null;
+
     public function register(Request $request)
     {
     	//Validate data
@@ -23,22 +26,39 @@ class ApiController extends Controller
 
         //Send failed response if request is not valid
         if ($validator->fails()) {
-            return response()->json(['error' => $validator->messages()], 200);
+
+            $this->error_msg = $validator->errors()->toArray();
+            $this->error_msg = array_values($this->error_msg)[0][0];
+
+            $this->response = [
+                "status" => "error",
+                "error" => true,
+                "response_code" => 400,
+                "message" => $this->error_msg,
+            ];
+
+        } else {
+
+            //Request is valid, create new user
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password)
+            ]);
+
+            //User created, return success response
+
+            $this->response = [
+                "status" => "success",
+                "error" => false,
+                "response_code" => 200,
+                "message" => "User created successfully",
+                "data" => $user
+            ];
         }
 
-        //Request is valid, create new user
-        $user = User::create([
-        	'name' => $request->name,
-        	'email' => $request->email,
-        	'password' => bcrypt($request->password)
-        ]);
+        return response($this->response, $this->response['response_code']);
 
-        //User created, return success response
-        return response()->json([
-            'success' => true,
-            'message' => 'User created successfully',
-            'data' => $user
-        ], Response::HTTP_OK);
     }
 
     public function authenticate(Request $request)
@@ -53,31 +73,59 @@ class ApiController extends Controller
 
         //Send failed response if request is not valid
         if ($validator->fails()) {
-            return response()->json(['error' => $validator->messages()], 400);
+
+            $this->error_msg = $validator->errors()->toArray();
+            $this->error_msg = array_values($this->error_msg)[0][0];
+
+            $this->response = [
+                "status" => "error",
+                "error" => true,
+                "response_code" => 400,
+                "message" => $this->error_msg,
+            ];
+
+            return response($this->response, $this->response['response_code']);
         }
 
         //Request is validated
         //Crean token
         try {
             if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json([
-                	'success' => false,
-                	'message' => 'Login credentials are invalid.',
-                ], 400);
+
+                $this->response = [
+                    "status" => "error",
+                    "error" => true,
+                    "response_code" => 400,
+                    "message" => 'Login credentials are invalid.',
+                ];
+
+                return response($this->response, $this->response['response_code']);
+
             }
         } catch (JWTException $e) {
     	return $credentials;
-            return response()->json([
-                	'success' => false,
-                	'message' => 'Could not create token.',
-                ], 500);
+            $this->response = [
+                "status" => "error",
+                "error" => true,
+                "response_code" => 500,
+                "message" => 'Could not create token.',
+            ];
+
+            return response($this->response, $this->response['response_code']);
         }
 
  		//Token created, return with success response and jwt token
-        return response()->json([
-            'success' => true,
-            'token' => $token,
-        ]);
+
+         $this->response = [
+            "status" => "success",
+            "error" => false,
+            "response_code" => 200,
+            "message" => 'Success.',
+            "token" => $token,
+        ];
+
+        return response($this->response, $this->response['response_code']);
+
     }
 
     public function logout(Request $request)
